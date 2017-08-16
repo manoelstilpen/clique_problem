134f9d3855ebf7f636b4aed81db427a7cb66c6ac
Manoel Stilpen <mstilpen15@gmail.com>
2017-08-15 21:00:15 -0300

    relatorio atualizado

--- main.c
+++ main.c
@@ -180,6 +180,49 @@ short int testa_clique(Graph* graph, int* vertex, int tam){
 	return TRUE;
 }
 
+void swap(int* a, int* b)	{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+int partition (Graph* graph,int arr[], int low, int high){
+    int pivot = arr[high];   // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++){
+        // If current element is smaller than or
+        // equal to pivot
+//        if (graph->nAdjacencies[arr[j]] >= graph->nAdjacencies[pivot]){
+	    if (graph->nAdjacencies[arr[j]] >= graph->nAdjacencies[pivot]){
+            i++;    // increment index of smaller  element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+ 
+/*
+The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low  --> Starting index,
+high  --> Ending index
+*/
+void quickSort(Graph* graph, int arr[], int low, int high){
+    if (low < high){
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(graph, arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(graph, arr, low, pi - 1);
+        quickSort(graph, arr, pi + 1, high);
+    }
+}
+
 void new_algorithm_recursive(Graph* graph, int* vertex_list, int tam_list, int quant_vertex, short int* found, int *max){
 
 	if(tam_list == 0){
@@ -267,48 +310,6 @@ int new_algorithm(Graph* graph){
 	return max;
 }
 
-void swap(int* a, int* b)	{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
-int partition (Graph* graph,int arr[], int low, int high){
-    int pivot = arr[high];   // pivot
-    int i = (low - 1);  // Index of smaller element
- 
-    for (int j = low; j <= high- 1; j++){
-        // If current element is smaller than or
-        // equal to pivot
-//        if (graph->nAdjacencies[arr[j]] >= graph->nAdjacencies[pivot]){
-	    if (graph->nAdjacencies[arr[j]] >= graph->nAdjacencies[pivot]){
-            i++;    // increment index of smaller  element
-            swap(&arr[i], &arr[j]);
-        }
-    }
-
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
- 
-/*
-The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low  --> Starting index,
-high  --> Ending index
-*/
-void quickSort(Graph* graph, int arr[], int low, int high){
-    if (low < high){
-        /* pi is partitioning index, arr[p] is now
-           at right place */
-        int pi = partition(graph, arr, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(graph, arr, low, pi - 1);
-        quickSort(graph, arr, pi + 1, high);
-    }
-}
 
 int independent_set(Graph* graph){
 
